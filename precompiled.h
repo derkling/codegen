18b591fd9e328b4f5d6fba778f8d678bacadd3dd
Patrick Bellasi <derkling@gmail.com>
2014-04-14 12:27:20 +0200

    [Headers] Clean-up pre-compiled headers
    The headers to be pre-compiled are not just these regarding product
    generated entitied: i.e. Steps, SyncBarriers and PCBs.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- precompiled.h
+++ precompiled.h
@@ -1,24 +1,5 @@
 
-#include <chrono>
-#include <condition_variable>
-#include <memory>
-#include <mutex>
-#include <string>
-#include <thread>
-#include <vector>
-
-#include <cstdio>
-#include <stdint.h>
-#include <dlfcn.h>
-#include <sys/syscall.h>
-#include <unistd.h>
-
-#include <rapidxml.hpp>
-
 #include <demo_steps.h>
 #include <sync_barrier.h>
 #include <program_control_block.h>
-
-// Pre-allocate SyncBarrier templates
-SyncBarrier<int *> IntBarrier(nullptr, nullptr, "__pcIntBarrier", 0);
 
